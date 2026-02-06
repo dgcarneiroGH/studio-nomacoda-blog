459d2487ac0584751a2ba20899f2ba4bfa1b7203
Diego Carneiro <clarcat.ti@clarcat.com>
2026-02-06 09:17:48 +0100

    fix: update appId for correct deployment configuration in sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -10,7 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'tp706jbtjg8lrjyycra5mlyq',
     autoUpdates: true,
-    appId: 'k00he79gey74dc9sy20l38ui',
   },
 })
